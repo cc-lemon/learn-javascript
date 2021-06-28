cf2a0e89e76da35aec4b7be32b1643890d13a29d
cc-lemon <christiancroona@gmail.com>
2021-06-28 17:32:11 +0200

    Five first challenges

--- 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
+++ 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
@@ -1,10 +1,12 @@
 // document.getElementById("count").innerText = 5
+//let count be zero
+let count = 0;
 
-let count = 0
+console.log(count);
 
-console.log(count)
 
 
 // 1. Create a variable, myAge, and set its value to your age
-
+let myAge = 31;
 // 2. Log the myAge variable to the console
+console.log(myAge);
